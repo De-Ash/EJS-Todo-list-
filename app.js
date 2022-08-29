0c510c06394dfea15e628232a7a99f93e31109a7
De-Ash <whytewayne521@gmail.com>
2022-08-29 14:35:21 -0700

    Add control flow to ejs file and apply css, server now has logic for date parsing

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 // jshint esversion:6
 const express=require("express");
 const bodyParser=require("body-parser");
-
 const app=express();
+
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("Public"));
 app.set("view engine","ejs");
 app.get("/",(req,res)=>{
-    var day= new Date();
-    var today= day.getDay;
-    if ((today==6)||(today==0)) {
-        res.send("its the weekend yay!");
-    } else {
-        res.send("its the weekday...get to work");
-    }
-    // app.render();
+    var date= new Date();
+    var today= date.getDay();
+    var weekDays=["Sun","Mon","Tues","Wednes","Thurs","Fri","Satur"];
+    var day= weekDays[today]+"day";
+    res.render("list",{kindaDay:day});
 });
 app.listen(3000,()=>{
     console.log("Ash kun.. new server started at post 3000");
