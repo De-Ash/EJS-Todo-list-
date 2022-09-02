88c429391a9afad261bdd56702a4df56ef72b9b2
De-Ash <whytewayne521@gmail.com>
2022-09-02 11:09:53 -0700

    1. Add work route 2. Implement logic to redirect updated array 3. Implement logic to redirect  rendered page

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser=require("body-parser");
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:true}));
-let newItems=["first item","second item"];
+let newItems=[];
+let workItems=[];
 app.use(express.static("Public"));
 app.set("view engine","ejs");
 app.get("/",(req,res)=>{
@@ -14,15 +15,24 @@ app.get("/",(req,res)=>{
         day:"numeric",
         month:"short",
     };
-    let today=date.toLocaleDateString("en-US",Options);
-    let day=today;
-    res.render("list",{kindaDay:day,newListItems:newItems});
+    let day=date.toLocaleDateString("en-US",Options);
+    let today=day;
+        res.render("list",{listTitle:today,newListItems:newItems});
 });
 app.post("/",(req,res)=>{
     let item=req.body.item;
-    newItems.push(item);
-    res.redirect("/");
+    let btn = req.body.submit;
+    if (btn=="Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        newItems.push(item);
+        res.redirect("/");
+    }
+});
+app.get("/work",(req,res)=>{
+    res.render("list",{listTitle:"Work List",newListItems:workItems});
 });
 app.listen(3000,()=>{
-    console.log("Ash kun.. new server started at post 3000");
+    console.log("Ash kun.. new server started at port 3000");
 });
